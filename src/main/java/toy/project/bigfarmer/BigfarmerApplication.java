97a7e06e92a5c7bc3d97e80c81135564cffe1a95
kdgiant174 <kdgaint174@gmail.com>
2019-02-10 19:01:03 +0900

    project setting

---
+++ src/main/java/toy/project/bigfarmer/BigfarmerApplication.java
@@ -0,0 +1,14 @@
+package toy.project.bigfarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfarmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigfarmerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/toy/project/bigfarmer/BigfarmerApplicationTests.java
@@ -0,0 +1,17 @@
+package toy.project.bigfarmer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BigfarmerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
